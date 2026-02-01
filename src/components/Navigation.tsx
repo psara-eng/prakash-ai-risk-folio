e0f3eba72afe03f18559d14b1354d6f3857a9c35
psara-eng <prakashsarangi.ps@gmail.com>
2026-02-01 22:53:02 +0530

    Update Navigation.tsx
    remove blog

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,7 +11,7 @@ const Navigation = () => {
     { path: "/", label: "Home" },
     { path: "/about", label: "About" },
     { path: "/projects", label: "Projects" },
-    { path: "/blog", label: "Blog" },
+    {/*{ path: "/blog", label: "Blog" },*/}
     { path: "/contact", label: "Contact" },
   ];
 
